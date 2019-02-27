6ede33caa807b5a8670f4e58502ac60785cdd0c9
Long Phan <long@macbook.local>
2019-02-27 13:21:01 +0700

    Init new task page

---
+++ lib/pages/new_task.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:fpomodoro/models/task.dart';
+
+class NewTaskPage extends StatefulWidget {
+  NewTaskPage({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _NewTaskPageState();
+}
+
+class _NewTaskPageState extends State<NewTaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
